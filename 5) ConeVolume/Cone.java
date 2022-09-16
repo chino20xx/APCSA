e1f9c1f5f9a9c69057490df6792430be947168a1
William Qu <91852939+chino20xx@users.noreply.github.com>
2022-09-15 21:38:34 -0500

    Add files via upload

---
+++ 5) ConeVolume/Cone.java
@@ -0,0 +1,11 @@
+
+public class Cone
+{
+	public static double volume( double radius, int height)
+	{	
+    
+
+		double volume = (1.0/3)*3.14159*(Math.pow(radius, 2))*height;
+    return volume;
+	}
+}
---
+++ 5) ConeVolume/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.printf( "%.2f\n", Cone.volume( 4, 4) );
+		System.out.printf( "%.2f\n", Cone.volume( 4, 3) );
+		System.out.printf( "%.2f\n", Cone.volume( 9, 3) );
+		System.out.printf( "%.2f\n", Cone.volume( 1, 3) );
+		System.out.printf( "%.2f\n", Cone.volume( 1, 5) );
+		System.out.printf( "%.2f\n", Cone.volume( 7, 7) );
+		System.out.printf( "%.2f\n", Cone.volume( 1.5, 3) );
+		System.out.printf( "%.2f\n", Cone.volume( 1.5, 5) );
+  }
+}
